cab22e8f046007c0c32fd68ddf2719ad67420227
bcepnez <senju_mr@hotmail.com>
2018-03-23 13:19:21 +0700

    modified porgram#2.c & create program#3.c

---
+++ Program#3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Who are you? : ");
+	scanf("%s",&name);
+	printf("Hi %s.",name);
+	return 0;
+}
--- program#2.c
+++ program#2.c
@@ -2,5 +2,6 @@
 
 int main()
 {
-	printf("1 2 3 4 5 6 7 8 9");
+	printf("A : 1 2 3 4 5 6 7 8 9");
+	printf("\nB : Count for what?");
 }
